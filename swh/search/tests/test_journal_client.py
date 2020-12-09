c7cd341796369521381ebcfba7dba0695bfadf6a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-12-09 22:32:29 +0100

    test_journal_client: Migrate to pytest

--- swh/search/tests/test_journal_client.py
+++ swh/search/tests/test_journal_client.py
@@ -1,71 +1,69 @@
-# Copyright (C) 2019  The Software Heritage developers
+# Copyright (C) 2019-2020  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 import functools
-import unittest
 from unittest.mock import MagicMock
 
 from swh.search.journal_client import process_journal_objects
 
 
-class SearchJournalClientTest(unittest.TestCase):
-    def test_origin_from_journal(self):
-        search_mock = MagicMock()
+def test_journal_client_origin_from_journal():
+    search_mock = MagicMock()
 
-        worker_fn = functools.partial(process_journal_objects, search=search_mock,)
+    worker_fn = functools.partial(process_journal_objects, search=search_mock,)
 
-        worker_fn({"origin": [{"url": "http://foobar.baz"},]})
-        search_mock.origin_update.assert_called_once_with(
-            [{"url": "http://foobar.baz"},]
-        )
+    worker_fn({"origin": [{"url": "http://foobar.baz"},]})
+    search_mock.origin_update.assert_called_once_with(
+        [{"url": "http://foobar.baz"},]
+    )
 
-        search_mock.reset_mock()
+    search_mock.reset_mock()
 
-        worker_fn(
-            {"origin": [{"url": "http://foobar.baz"}, {"url": "http://barbaz.qux"},]}
-        )
-        search_mock.origin_update.assert_called_once_with(
-            [{"url": "http://foobar.baz"}, {"url": "http://barbaz.qux"},]
-        )
+    worker_fn({"origin": [{"url": "http://foobar.baz"}, {"url": "http://barbaz.qux"},]})
+    search_mock.origin_update.assert_called_once_with(
+        [{"url": "http://foobar.baz"}, {"url": "http://barbaz.qux"},]
+    )
 
-    def test_origin_visit_from_journal(self):
-        search_mock = MagicMock()
 
-        worker_fn = functools.partial(process_journal_objects, search=search_mock,)
+def test_journal_client_origin_visit_from_journal():
+    search_mock = MagicMock()
 
-        worker_fn({"origin_visit": [{"origin": {"url": "http://foobar.baz"},}]})
-        search_mock.origin_update.assert_called_once_with(
-            [{"url": "http://foobar.baz", "has_visits": True},]
-        )
+    worker_fn = functools.partial(process_journal_objects, search=search_mock,)
 
-    def test_origin_metadata_from_journal(self):
-        search_mock = MagicMock()
+    worker_fn({"origin_visit": [{"origin": {"url": "http://foobar.baz"},}]})
+    search_mock.origin_update.assert_called_once_with(
+        [{"url": "http://foobar.baz", "has_visits": True},]
+    )
 
-        worker_fn = functools.partial(process_journal_objects, search=search_mock,)
 
-        worker_fn(
-            {
-                "origin_intrinsic_metadata": [
-                    {
-                        "id": "http://foobar.baz",
-                        "metadata": {
-                            "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
-                            "description": "foo bar",
-                        },
-                    },
-                ]
-            }
-        )
-        search_mock.origin_update.assert_called_once_with(
-            [
+def test_journal_client_origin_metadata_from_journal():
+    search_mock = MagicMock()
+
+    worker_fn = functools.partial(process_journal_objects, search=search_mock,)
+
+    worker_fn(
+        {
+            "origin_intrinsic_metadata": [
                 {
-                    "url": "http://foobar.baz",
-                    "intrinsic_metadata": {
+                    "id": "http://foobar.baz",
+                    "metadata": {
                         "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
                         "description": "foo bar",
                     },
                 },
             ]
-        )
+        }
+    )
+    search_mock.origin_update.assert_called_once_with(
+        [
+            {
+                "url": "http://foobar.baz",
+                "intrinsic_metadata": {
+                    "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
+                    "description": "foo bar",
+                },
+            },
+        ]
+    )
