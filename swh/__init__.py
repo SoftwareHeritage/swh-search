3e129a3f48eee21d6c0a308eb35d749a3df21d95
Antoine Lambert <antoine.lambert@inria.fr>
2021-06-09 15:08:47 +0200

    mypy: Fix errors with release >= v0.900

--- swh/__init__.py
+++ swh/__init__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2019-2020  The Software Heritage developers
+# Copyright (C) 2019-2021  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 from pkgutil import extend_path
-from typing import Iterable
+from typing import List
 
-__path__: Iterable[str] = extend_path(__path__, __name__)
+__path__: List[str] = extend_path(__path__, __name__)
