2edbbbe833e9e3e1d77231396c54095839bbcc13
KShivendu <shivendu@iitbhilai.ac.in>
2021-07-13 16:21:51 +0530

    query_language: Setup tree-sitter and grammar.js
    This revision defines the grammar for the search query language and
    prepares swh.search for a smoother development of the grammar.
    
    The parsers generated from the proposed grammar serve two different purposes:
    - Translation of search queries into elasticsearch DSL in swh.search (or any
    other search backend that we may use in the future)
    - Autocompletion of the queries in the swh.web (Archive UI)
    
    tree-sitter has been selected for the task because it has bindings for
    python (swh.search) as well as wasm (swh.web).

---
+++ query_language/build.py
@@ -0,0 +1,3 @@
+from tree_sitter import Language
+
+Language.build_library("swh_ql.so", ["."])
--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from distutils.cmd import Command
+from distutils.command.build import build
 from io import open
-from os import path
+from os import environ, path, system
 
 from setuptools import find_packages, setup
 
@@ -33,6 +35,76 @@ def parse_requirements(name=None):
                 continue
             requirements.append(line)
     return requirements
+
+
+yarn = environ.get("YARN", "yarn")
+
+
+class TSCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class TSInstallCommand(TSCommand):
+    description = "Installs node_modules related to query language"
+
+    def run(self):
+        system(f"{yarn} install")
+
+
+class TSGenerateCommand(TSCommand):
+    description = "Generates parser related files from grammar.js"
+
+    def run(self):
+        system(f"{yarn} generate")
+
+
+class TSBuildSoCommand(TSCommand):
+    description = "Builds swh_ql.so"
+
+    def run(self):
+        system(f"{yarn} build-so && echo 'swh_ql.so file generated'")
+
+
+class TSBuildWasmCommand(TSCommand):
+    description = "Builds swh_ql.wasm"
+
+    def run(self):
+        system(f"{yarn} build-wasm && echo 'swh_ql.wasm file generated'")
+
+
+class TSBuildCommand(TSCommand):
+    description = "Builds swh_ql.so and swh_ql.wasm"
+
+    def run(self):
+        self.run_command("ts_build_so")
+        self.run_command("ts_build_wasm")
+
+
+class TSBuildExportCommand(TSCommand):
+    description = "Builds swh_ql.so and swh_ql.wasm and exports them to static/"
+
+    def run(self):
+        self.run_command("ts_install")
+        self.run_command("ts_build")
+
+        system("echo 'static files generated. copying them to static/ dir'")
+        system("mkdir static")
+        system("cp query_language/swh_ql.so static/swh_ql.so")
+        system("cp query_language/swh_ql.wasm static/swh_ql.wasm")
+
+
+class custom_build(build):
+    def run(self):
+        if not self.dry_run:
+            self.run_command("ts_build_export")
+
+        super().run()
 
 
 setup(
@@ -68,4 +140,14 @@ setup(
         "Source": "https://forge.softwareheritage.org/source/swh-search",
         "Documentation": "https://docs.softwareheritage.org/devel/swh-search/",
     },
+    cmdclass={
+        "build": custom_build,
+        "ts_install": TSInstallCommand,
+        "ts_generate": TSGenerateCommand,
+        "ts_build_so": TSBuildSoCommand,
+        "ts_build_wasm": TSBuildWasmCommand,
+        "ts_build": TSBuildCommand,
+        "ts_build_export": TSBuildExportCommand,
+    },
+    data_files=[("share/swh/search", ["static/swh_ql.so", "static/swh_ql.wasm"])],
 )
