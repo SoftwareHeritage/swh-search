5e77f2aa97a9999a977d1fa9e8f5a5f6f5a1d226
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-09-23 19:45:04 +0200

    conftest: Support Elasticsearch 8
    The configuration also works for ES 7, so we can just vary the way that
    the ES server is started to support both major versions.

--- swh/search/tests/conftest.py
+++ swh/search/tests/conftest.py
@@ -1,11 +1,13 @@
-# Copyright (C) 2019-2021  The Software Heritage developers
+# Copyright (C) 2019-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 import logging
+from pathlib import Path
 import socket
 import subprocess
+import textwrap
 import time
 
 import elasticsearch
@@ -24,71 +26,121 @@ def free_port():
     return port
 
 
-CONFIG_TEMPLATE = """
-node.name: node-1
-path.data: {data}
-path.logs: {logs}
-network.host: 127.0.0.1
-http.port: {http_port}
-transport.port: {transport_port}
-"""
+CONFIG_TEMPLATES = {
+    "elasticsearch.yml": """\
+        node.name: node-1
+        path.data: {data}
+        path.logs: {logs}
+        network.host: 127.0.0.1
+        http.port: {http_port}
+        transport.port: {transport_port}
+        xpack.security.enabled: false
+        discovery.type: single-node
+        cluster.routing.allocation.disk.threshold_enabled: false
+    """,
+    "jvm.options": """\
+        -XX:+UseG1GC
+        -Djava.io.tmpdir={data}
+        20-:--add-modules=jdk.incubator.vector
+        -XX:+HeapDumpOnOutOfMemoryError
+        -XX:+ExitOnOutOfMemoryError
+        -XX:HeapDumpPath={logs}
+        -XX:ErrorFile={logs}/hs_err_pid%p.log
+        -Xlog:gc*,gc+age=trace,safepoint:file={logs}/gc.log:utctime,level,pid,tags:filecount=32,filesize=64m
+    """,
+    "log4j2.properties": """\
+        status = error
+
+        appender.console.type = Console
+        appender.console.name = console
+        appender.console.layout.type = PatternLayout
+        appender.console.layout.pattern = [%d{{ISO8601}}][%-5p][%-25c{{1.}}] [%node_name]%marker %m%consoleException%n
+
+        rootLogger.level = info
+        rootLogger.appenderRef.console.ref = console
+    """,  # noqa
+}
+
+ES_HOME = "/usr/share/elasticsearch"
+
+
+def _get_es_version():
+    """Parse /usr/share/elasticsearch/lib/elasticsearch-X.Y.Z.jar into X"""
+    jars = list(Path(f"{ES_HOME}/lib").glob("elasticsearch-[0-9]*.jar"))
+    if len(jars) != 1:
+        raise ValueError(f"Found unexpected contents in {ES_HOME}/lib: {jars}")
+
+    jarfile = jars[0]
+    major = jarfile.stem.split("-")[-1].split(".")[0]
+    return int(major)
 
 
 def _run_elasticsearch(
     conf_dir, data_dir, logs_dir, http_port, transport_port, libffi_tmpdir
 ):
-    es_home = "/usr/share/elasticsearch"
-
-    with open(conf_dir + "/elasticsearch.yml", "w") as fd:
-        fd.write(
-            CONFIG_TEMPLATE.format(
-                data=data_dir,
-                logs=logs_dir,
-                http_port=http_port,
-                transport_port=transport_port,
+    for filename, template in CONFIG_TEMPLATES.items():
+        with open(f"{conf_dir}/{filename}", "w") as fd:
+            fd.write(
+                textwrap.dedent(template).format(
+                    data=data_dir,
+                    logs=logs_dir,
+                    http_port=http_port,
+                    transport_port=transport_port,
+                )
             )
-        )
 
-    with open(conf_dir + "/log4j2.properties", "w") as fd:
-        pass
+    major = _get_es_version()
+    if major >= 8:
+        cmd = [
+            f"{ES_HOME}/jdk/bin/java",
+            "-Dcli.name=server",
+            f"-Dcli.script={ES_HOME}/bin/elasticsearch",
+            "-Dcli.libs=lib/tools/server-cli",
+            f"-Des.path.home={ES_HOME}",
+            f"-Des.path.conf={conf_dir}",
+            "-Des.distribution.type=deb",
+            "-Des.bundled_jdk=true",
+            "-cp",
+            f"{ES_HOME}/lib/*:{ES_HOME}/lib/cli-launcher/*",
+            "org.elasticsearch.launcher.CliToolLauncher",
+        ]
+    else:
+        cmd = [
+            f"{ES_HOME}/jdk/bin/java",
+            f"-Des.path.home={ES_HOME}",
+            f"-Des.path.conf={conf_dir}",
+            "-Des.bundled_jdk=true",
+            "-Dlog4j2.disable.jmx=true",
+            # fix UnsupportedOperationException related to deprecated Security Manager
+            # when using JDK >= 18
+            "-Djava.security.manager=allow",
+            "-cp",
+            f"{ES_HOME}/lib/*",
+            "org.elasticsearch.bootstrap.Elasticsearch",
+        ]
 
-    cmd = [
-        "/usr/share/elasticsearch/jdk/bin/java",
-        "-Des.path.home={}".format(es_home),
-        "-Des.path.conf={}".format(conf_dir),
-        "-Des.bundled_jdk=true",
-        "-Dlog4j2.disable.jmx=true",
-        # fix UnsupportedOperationException related to deprecated Security Manager
-        # when using JDK >= 18
-        "-Djava.security.manager=allow",
-        "-cp",
-        "{}/lib/*".format(es_home),
-        "org.elasticsearch.bootstrap.Elasticsearch",
-    ]
-
-    host = "127.0.0.1:{}".format(http_port)
-
-    with open(logs_dir + "/output.txt", "w") as fd:
-        p = subprocess.Popen(cmd, cwd=es_home, env={"LIBFFI_TMPDIR": libffi_tmpdir})
+    process = subprocess.Popen(cmd, cwd=ES_HOME, env={"LIBFFI_TMPDIR": libffi_tmpdir})
 
     for _ in range(3000):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(("127.0.0.1", http_port))
         except ConnectionRefusedError:
-            if p.poll() is not None:
-                # process ahs exited
+            if process.poll() is not None:
+
+                # process has exited
                 break
-            else:
-                time.sleep(0.1)
+
+            time.sleep(0.1)
         else:
             sock.close()
             break
 
+    host = f"127.0.0.1:{http_port}"
     client = elasticsearch.Elasticsearch([host])
     assert client.ping()
 
-    return p
+    return process, major
 
 
 @pytest.fixture(scope="session")
@@ -99,7 +151,7 @@ def elasticsearch_session(tmpdir_factory):
     http_port = free_port()
     transport_port = free_port()
 
-    p = _run_elasticsearch(
+    proc, major = _run_elasticsearch(
         conf_dir=str(es_conf),
         data_dir=str(tmpdir.mkdir("data")),
         logs_dir=str(tmpdir.mkdir("logs")),
@@ -111,10 +163,12 @@ def elasticsearch_session(tmpdir_factory):
     yield "127.0.0.1:{}".format(http_port)
 
     # Check ES didn't stop
-    assert p.returncode is None, p.returncode
+    assert proc.returncode is None, proc.returncode
 
-    p.terminate()
-    p.wait()
+    proc.terminate()
+    if major < 8:
+        proc.kill()
+    proc.wait()
 
 
 @pytest.fixture(scope="class")
